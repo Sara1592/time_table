20dd4305b7425970ee8ecee59104570964ddac13
saranya <117970577+Sara1592@users.noreply.github.com>
2023-08-30 11:47:26 +0530

    select_button_validator

--- lib/view/components/period_allocation.dart
+++ lib/view/components/period_allocation.dart
@@ -143,9 +143,11 @@ class _PeriodAllocationState extends State<PeriodAllocation> {
                                         child: Text(
                                           dayOrder_1[index]['sub_name']
                                               .toString(),
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold),
+                                          style: GoogleFonts.montserrat(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
                                         ),
                                       )),
                           );
